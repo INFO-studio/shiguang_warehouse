4efb80d931be107fa3e0f00430f7e641279ba8d6
茵符草 <beta-s@outlook.com>
2025-11-22 22:57:18 +0800

    feat: cqu

--- resources/GLOBAL_TOOLS/test.js
+++ resources/GLOBAL_TOOLS/test.js
@@ -18,7 +18,7 @@ const baseFetch = async (url, accessToken, method, body, description) => {
         }
     )
     if (!response.ok) {
-        AndroidBridge.showToast(`获取${description}失败: ${termResponse.status} ${termResponse.statusText}`)//, `获取${description}失败，请退出重试`)
+        AndroidBridge.showToast(`获取${description}失败，请退出重试`)
         throw new Error(`获取${description}失败: ${termResponse.status} ${termResponse.statusText}`)
     }
     return await response.json()
@@ -28,7 +28,7 @@ const getTermId = async (accessToken) => (await baseFetch('https://my.cqu.edu.cn
 
 const getStartDate = async (termId, accessToken) => (new Date((await baseFetch(`https://my.cqu.edu.cn/api/resourceapi/session/info/${termId}`, accessToken, 'GET', null, '学期详情')).data.beginDate).toISOString().split('T')[0])
 
-const getMaxWeek = async (termId, accessToken) => await baseFetch(`https://my.cqu.edu.cn/api/timetable/course/maxWeek/${termId}`, accessToken, 'GET', null, '最大周数')
+const getMaxWeek = async (termId, accessToken) => (await baseFetch(`https://my.cqu.edu.cn/api/timetable/course/maxWeek/${termId}`, accessToken, 'GET', null, '最大周数')).data
 
 const getTimeSlots = async (accessToken) => (await baseFetch('https://my.cqu.edu.cn/api/workspace/time-pattern/session-time-pattern', accessToken, 'GET', null, '时间段配置')).data.classPeriodVOS
 
@@ -43,20 +43,7 @@ const parseSchedule = (startDate, maxWeek, timeSlots, schedule) => console.log(t
         number: timeSlot.periodOrder ?? index + 1,
         startTime: timeSlot.startTime ?? '',
         endTime: timeSlot.endTime ?? '',
-    })) + [
-        {number: 1, startTime: "08:30", endTime: "09:15"},
-        {number: 2, startTime: "09:25", endTime: "10:10"},
-        {number: 3, startTime: "10:30", endTime: "11:15"},
-        {number: 4, startTime: "11:25", endTime: "12:10"},
-        {number: 5, startTime: "13:30", endTime: "14:15"},
-        {number: 6, startTime: "14:25", endTime: "15:10"},
-        {number: 7, startTime: "15:30", endTime: "16:15"},
-        {number: 8, startTime: "16:25", endTime: "17:10"},
-        {number: 9, startTime: "17:20", endTime: "18:05"},
-        {number: 10, startTime: "19:00", endTime: "19:45"},
-        {number: 11, startTime: "19:55", endTime: "20:40"},
-        {number: 12, startTime: "20:50", endTime: "21:35"},
-    ],
+    })),
     courses: schedule.map((course) => ({
         name: course.courseName ?? '',
         teacher: course.instructorName?.slice(0, course.instructorName?.indexOf('-')) ?? '',
@@ -68,13 +55,11 @@ const parseSchedule = (startDate, maxWeek, timeSlots, schedule) => console.log(t
     })),
 })
 
-const saveSchedule = (parsedSchedule) => {
-    return Promise.allSettled([
-        window.AndroidBridgePromise.saveCourseConfig(JSON.stringify(parsedSchedule?.courseConfig)),
-        window.AndroidBridgePromise.saveImportedCourses(JSON.stringify(parsedSchedule?.courses)),
-        window.AndroidBridgePromise.savePresetTimeSlots(JSON.stringify(parsedSchedule?.timeSlots)),
-    ])
-}
+const saveSchedule = (parsedSchedule) => Promise.allSettled([
+    window.AndroidBridgePromise.saveCourseConfig(JSON.stringify(parsedSchedule?.courseConfig)),
+    window.AndroidBridgePromise.saveImportedCourses(JSON.stringify(parsedSchedule?.courses)),
+    window.AndroidBridgePromise.savePresetTimeSlots(JSON.stringify(parsedSchedule?.timeSlots)),
+])
 
 (async () => {
     if (!checkLogin()) {
@@ -92,8 +77,6 @@ const saveSchedule = (parsedSchedule) => {
     }
 
     const termId = await getTermId(accessToken)
-
-    console.log(await Promise.allSettled([getStartDate(termId, accessToken), getMaxWeek(termId, accessToken), getTimeSlots(accessToken), getSchedule(termId, accessToken, studentId)]))
 
     await saveSchedule(parseSchedule(...(await Promise.allSettled([getStartDate(termId, accessToken), getMaxWeek(termId, accessToken), getTimeSlots(accessToken), getSchedule(termId, accessToken, studentId)])).map(result => result.value)))
 
